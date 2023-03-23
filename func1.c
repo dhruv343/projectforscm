91f936d2898c91deaeb34a6350c87172279bc4b0
dhruv343 <118008255+dhruv343@users.noreply.github.com>
2023-03-23 11:20:52 +0530

    Add files via upload

---
+++ func1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <math.h>
+
+void printhello();
+void printbye();
+int main(){
+    printhello();
+    printbye();
+    return 0;
+}
+void printhello(){
+    printf("hello\n");
+}
+void printbye(){
+    printf("good bye");
+}
